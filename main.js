6fbd0ac99bbe3e879de3c45419a97353012ea2e9
Bonnie Simon <bonniesimon@protonmail.com>
2020-01-16 01:35:35 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,57 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow} = require('electron');
+
+let win;
+function createWindow(){
+    win = new BrowserWindow({
+        title: 'ClippY',
+        show : false
+    });
+
+
+    //Open DevTools
+    win.webContents.openDevTools();
+
+    win.loadURL(url.format({
+        pathname : path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes : true
+    }))
+
+    //When WIndow closed
+    win.on('ready-to-show', ()=>{
+        win.show();
+        win.focus();
+    });
+
+    win.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null
+      })
+}
+
+
+app.on('ready',createWindow);
+
+
+/**COde needed for windows app to close */
+app.on('window-all-closed', () => {
+    if(process.platform != 'darwin'){
+        app.quit();
+    }
+});
+
+/**Code for mac apps to close in addition to above code */
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+      createWindow()
+    }
+  })
+
